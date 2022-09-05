016fd46520834249239c27c477a0d384a7d2b006
Miliyonayalew <miliayalew@gmail.com>
2022-09-05 23:40:46 +0300

    Fixed eslint error

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -3,13 +3,13 @@ const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.hamburger-menu');
 
 function show() {
-  menuItem.classList.toggle("visible");
-  closeMenu.style.display = "flex";
+  menuItem.classList.toggle('visible');
+  closeMenu.style.display = 'flex';
 }
 
 function close() {
-  menuItem.style.top = "-200%";
-  closeMenu.style.display = "none";
+  menuItem.style.top = '-200%';
+  closeMenu.style.display = 'none';
 }
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
