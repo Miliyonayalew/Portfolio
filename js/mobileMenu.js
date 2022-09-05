247e3591d81229a1a657a6eb8fc424d14561ad8c
Edozie <edoziechukwuma@gmail.com>
2022-09-05 21:01:14 +0100

    Update mobileMenu.js

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -3,12 +3,13 @@ const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.hamburger-menu');
 
 function show() {
-  menuItem.style.display = 'flex';
-  menuItem.style.top = '0';
+  menuItem.classList.toggle("visible");
+  closeMenu.style.display = "flex";
 }
 
 function close() {
-  menuItem.style.top = '-200%';
+  menuItem.style.top = "-200%";
+  closeMenu.style.display = "none";
 }
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
