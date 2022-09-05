84a7e4245da0cca26d09597018efca03abd45fb9
hopeforkoami <hopeforkoami@gmail.com>
2022-09-05 19:32:19 +0000

    js update
    js update

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,1 +1,14 @@
+const menuItem = document.querySelector('.item-navigation');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.hamburger-menu');
 
+function show() {
+  menuItem.style.display = 'flex';
+  menuItem.style.top = '0';
+}
+
+function close() {
+  menuItem.style.top = '-200%';
+}
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
