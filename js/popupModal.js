5bc268d6cb0f73f900e561a3703db5171f6e29e8
hopeforkoami <hopeforkoami@gmail.com>
2022-09-07 14:59:50 +0000

    js modal update
    js modal update

---
+++ js/popupModal.js
@@ -0,0 +1,21 @@
+const myModal = document.getElementById('myModal');
+const seeProject = document.querySelectorAll('.btn-project');
+const closeMod = document.querySelector('.closeModal');
+const Projects = [
+  {
+    name: 'Keeping track of hundreds of components',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
+    image: 'Assests/ProjectImages/Project2.png',
+    technologies: [
+      'Codekit',
+      'GitHub',
+      'JavaScript',
+      'Bootstrap',
+      'Terminal',
+      'Codepen',
+    ],
+    Live: 'https://miliyonayalew.github.io/Portfolio/',
+    Source: 'https://github.com/Miliyonayalew/Portfolio',
+  },
+];
